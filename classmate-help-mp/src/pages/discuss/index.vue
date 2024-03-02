ebfef7896d2d1156e08c1c0099bd7ce3b2f91ee1
郑裕城 <z13538928222@163.com>
2024-03-02 19:26:12 +0800

    feat: 依赖安装 & 公告列表样式

--- classmate-help-mp/src/pages/discuss/index.vue
+++ classmate-help-mp/src/pages/discuss/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <view class="index"> DISCUSSS </view>
+  <view class="index"></view>
 </template>
 
 <script setup lang="ts"></script>
--- classmate-help-mp/src/pages/notice/index.vue
+++ classmate-help-mp/src/pages/notice/index.vue
@@ -1,7 +1,45 @@
 <template>
-  <view class="index"> NOTICE </view>
+  <view class="notice-box">
+    <uni-section title="公告列表" type="line">
+      <uni-collapse accordion>
+        <uni-collapse-item title="2024.03.02公告">
+          <view class="content">
+            <text class="text"
+              >2024.03.02公告同时只会保留一个组件的打开状态，其余组件会自动关闭。</text
+            >
+          </view>
+        </uni-collapse-item>
+        <uni-collapse-item title="2024.02.24公告">
+          <view class="content">
+            <text class="text"
+              >2024.03.02公告同时只会保留一个组件的打开状态，其余组件会自动关闭。</text
+            >
+          </view>
+        </uni-collapse-item>
+        <uni-collapse-item title="2024.01.14公告">
+          <view class="content">
+            <text class="text"
+              >2024.03.02公告同时只会保留一个组件的打开状态，其余组件会自动关闭。</text
+            >
+          </view>
+        </uni-collapse-item>
+      </uni-collapse>
+    </uni-section>
+  </view>
 </template>
 
 <script setup lang="ts"></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.notice-box {
+  height: 90%;
+  padding: 30rpx;
+  .content {
+    padding: 30rpx;
+    .text {
+      padding-left: 36rpx;
+      font-size: 36rpx;
+    }
+  }
+}
+</style>
