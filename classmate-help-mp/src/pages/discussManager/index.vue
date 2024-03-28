2d03b83d8a94a2112330ef8230509946d96627e4
郑裕城 <yucheng_z@gzpionner.cn>
2024-03-28 16:19:09 +0800

    feat:评论按钮

--- classmate-help-mp/src/pages/discussManager/index.vue
+++ classmate-help-mp/src/pages/discussManager/index.vue
@@ -1,8 +1,91 @@
 <template>
-	<view class="index"> dism </view>
-  </template>
-  
-  <script setup lang="ts"></script>
-  
-  <style lang="scss" scoped></style>
-  
+  <view class="index">
+    <uni-section title="展示中" type="line">
+      <view class="discuss-list">
+        <view class="card" v-for="item in list" :key="item.id" @click="openDetail(item.id)">
+          <view class="pContent">{{ item.pContent }}</view>
+          <view class="common">
+            <image src="@/static/i-common.png"></image>
+            <view class="common-count">{{ item.commonCount }}</view>
+          </view>
+        </view>
+      </view>
+    </uni-section>
+    <view class="placeholder"></view>
+    <uni-section title="不展示" type="line">
+      <view class="discuss-list">
+        <view class="card" v-for="item in list" :key="item.id" @click="openDetail(item.id)">
+          <view class="pContent">{{ item.pContent }}</view>
+          <view class="common">
+            <image src="@/static/i-common.png"></image>
+            <view class="common-count">{{ item.commonCount }}</view>
+          </view>
+        </view>
+      </view>
+    </uni-section>
+  </view>
+</template>
+
+<script setup lang="ts">
+let list = [
+  {
+    id: 1,
+    pUser: "发表者",
+    pContent: "有人知道计算机三班的课表吗?",
+    commonCount: 23,
+  },
+  {
+    id: 2,
+    pUser: "sikuu",
+    pContent: "明天什么时候开学?",
+    commonCount: 19,
+  },
+];
+let openDetail = (id: number) => {
+  uni.navigateTo({ url: `/pages/discussDetail/index?id=${id}` });
+};
+</script>
+
+<style lang="scss" scoped>
+.discuss-list {
+  padding: 40rpx;
+  height: 90%;
+  display: flex;
+  flex-direction: column;
+  gap: 40rpx;
+  .card {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    background-color: rgba(241, 249, 250, 0.904);
+    border-radius: 10rpx;
+    box-shadow: 10rpx 5rpx 10rpx rgb(232, 232, 232);
+    padding: 10rpx 10rpx 30rpx 10rpx;
+    .pUser {
+      font-size: 48rpx;
+      font-weight: bolder;
+      text-indent: 0.4em;
+    }
+    .pContent {
+      margin-top: 10rpx;
+      font-size: 36rpx;
+      text-indent: 2em;
+    }
+    .common {
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+      padding-top: 10rpx;
+      padding-right: 40rpx;
+      image {
+        height: 50rpx;
+        width: 50rpx;
+      }
+    }
+  }
+}
+.placeholder {
+  height: 100rpx;
+  width: 100%;
+}
+</style>
--- classmate-help-mp/src/pages/shopDetail/index.vue
+++ classmate-help-mp/src/pages/shopDetail/index.vue
@@ -9,14 +9,7 @@
       </view>
       <view class="coast">￥30</view>
     </view>
-    <swiper
-      class="image-box"
-      circular
-      indicator-dots
-      autoplay
-      :interval="3000"
-      :duration="500"
-    >
+    <swiper class="image-box" circular indicator-dots autoplay :interval="3000" :duration="500">
       <swiper-item>
         <image class="image" src="@/static/photo1.png"></image>
       </swiper-item>
@@ -29,10 +22,13 @@
     </swiper>
 
     <view class="msg">
-      <view class="title">评论区</view>
-      <view class="msg-box">
+      <div class="top-box">
+        <view class="title">评论区</view>
+        <div class="common-btn" @click="openCommonDialog()">评论</div>
+      </div>
+      <view class="msg-box" @click="openCommonDialog(12)">
         <view class="user">sikuu</view>
-        <view class="content">我想要,可以商量价格吗?</view>
+        <view class="content">我想要,可以商1量价格吗?</view>
         <view class="date">2023-05-11</view>
       </view>
       <view class="msg-box">
@@ -46,43 +42,48 @@
         <view class="date">2023-01-11</view>
       </view>
     </view>
+    <view>
+      <!-- 输入框示例 -->
+      <uni-popup ref="inputDialog" type="dialog">
+        <uni-popup-dialog
+          ref="inputClose"
+          mode="input"
+          :title="dialogTitle"
+          :value="common"
+          placeholder="请输入内容"
+          @confirm="dialogInputConfirm"
+        ></uni-popup-dialog>
+      </uni-popup>
+    </view>
   </view>
+  <!-- 新增/编辑表单 -->
   <view class="edit-page" v-else-if="type === '0'">
     <form @submit="formSubmit">
       <view class="edit-part">
         <view class="title">商品名</view>
-        <input
-          class="line"
-          focus
-          placeholder="请输入商品名"
-          v-model="form.title"
-        />
+        <input class="line" focus placeholder="请输入商品名" v-model="form.title" />
       </view>
       <view class="edit-part">
         <view class="title">品类</view>
         <radio-group @change="radioChange">
-          <label class="radio"
-            ><radio value="0" :checked="form.radioType === '0'" />教材</label
-          >
-          <label class="radio"
-            ><radio value="1" :checked="form.radioType === '1'" />衣物</label
-          >
-          <label class="radio"
-            ><radio value="2" :checked="form.radioType === '2'" />电子</label
-          >
-          <label class="radio"
-            ><radio value="3" :checked="form.radioType === '3'" />其他</label
-          >
+          <label class="radio">
+            <radio value="0" :checked="form.radioType === '0'" />教材
+          </label>
+          <label class="radio">
+            <radio value="1" :checked="form.radioType === '1'" />衣物
+          </label>
+          <label class="radio">
+            <radio value="2" :checked="form.radioType === '2'" />电子
+          </label>
+          <label class="radio">
+            <radio value="3" :checked="form.radioType === '3'" />其他
+          </label>
         </radio-group>
       </view>
       <view class="edit-part">
         <view class="title">商品简介</view>
-        <textarea
-          v-model="form.intro"
-          placeholder="请输入商品简介"
-          class="line"
-          :style="{ height: '500rpx', width: '100%' }"
-        />
+        <textarea v-model="form.intro" placeholder="请输入商品简介" class="line"
+          :style="{ height: '500rpx', width: '100%' }" />
       </view>
       <view class="edit-part">
         <view class="title">商品图片</view>
@@ -90,18 +91,14 @@
           <view class="up-image" v-for="item in form.imgList" :key="item">
             <image :src="item"></image>
           </view>
-          <view
-            class="up-image"
-            @click="uploadImg()"
-            v-if="form.imgList.length < 3"
-            >+</view
-          >
+          <view class="up-image" @click="uploadImg()" v-if="form.imgList.length < 3">+</view>
         </view>
       </view>
       <view class="btns">
         <button type="primary" form-type="submit">提交</button>
       </view>
     </form>
+    
   </view>
 </template>
 
@@ -124,6 +121,24 @@ let form = ref<formType>({
   imgList: [],
 });
 
+// 评论弹窗相关
+let dialogTitle = ref("评论");
+let inputDialog = ref();
+let common = ref("");
+let openCommonDialog = (id?: number) => {
+  if (id) {
+    console.log('id', id)
+    dialogTitle.value = "回复评论";
+  } else {
+    dialogTitle.value = "评论";
+  }
+  inputDialog.value.open();
+};
+let dialogInputConfirm = (value: string) => {
+  console.log("提交:", value);
+
+  common.value = "";
+};
 onLoad((e) => {
   // 选择性赋值（读取路由参数）
   if (e?.type) type.value = e.type;
@@ -157,18 +172,21 @@ let uploadImg = () => {
   justify-content: flex-start;
   align-items: center;
   gap: 20rpx;
+
   .image-box {
     width: 100%;
     height: 600rpx;
     border-radius: 10rpx;
     background-color: white;
     box-shadow: 10rpx 5rpx 10rpx rgb(232, 232, 232);
+
     .image {
       width: 100%;
       height: 600rpx;
       border-radius: 10rpx;
     }
   }
+
   .card {
     width: 100%;
     height: 300rpx;
@@ -184,13 +202,15 @@ let uploadImg = () => {
       flex-direction: column;
       gap: 40rpx;
       width: 70%;
+
       .title {
         font-size: 48rpx;
         font-weight: bolder;
       }
-      .content {
-      }
+
+      .content {}
     }
+
     .coast {
       flex: 1;
       display: flex;
@@ -200,6 +220,7 @@ let uploadImg = () => {
       font-weight: bolder;
     }
   }
+
   .msg {
     width: 100%;
 
@@ -207,10 +228,27 @@ let uploadImg = () => {
     box-shadow: 10rpx 5rpx 10rpx rgb(232, 232, 232);
     padding: 20rpx;
     box-sizing: border-box;
-    .title {
-      font-size: 48rpx;
-      font-weight: bolder;
+
+    .top-box {
+      display: flex;
+      justify-content: space-between;
+      .title {
+        font-size: 48rpx;
+        font-weight: bolder;
+      }
+
+      .common-btn {
+        border: 1px solid gray;
+        border-radius: 10rpx;
+        padding: 10rpx 20rpx;
+
+        &:active {
+          background-color: rgb(230, 230, 230);
+        }
+      }
     }
+
+
     .msg-box {
       display: flex;
       flex-direction: column;
@@ -218,16 +256,21 @@ let uploadImg = () => {
       border-bottom: 1px black solid;
       padding: 10rpx;
       box-sizing: border-box;
+      &:active{
+        background-color: rgb(242, 242, 242);
+      }
       .user {
         font-size: 36rpx;
         font-weight: bold;
       }
+
       .date {
         align-self: flex-end;
       }
     }
   }
 }
+
 .edit-page {
   padding: 30rpx;
   display: flex;
@@ -235,24 +278,29 @@ let uploadImg = () => {
   justify-content: flex-start;
   align-items: center;
   gap: 20rpx;
+
   .edit-part {
     padding: 20rpx;
     display: flex;
     flex-direction: column;
     gap: 10rpx;
+
     .title {
       font-size: 48rpx;
       font-weight: bolder;
     }
+
     .line {
       height: 50rpx;
       font-size: 40rpx;
       background-color: white;
       box-shadow: 10rpx 5rpx 10rpx rgb(232, 232, 232);
     }
+
     .radio {
       font-size: 36rpx;
     }
+
     .upload-image-box {
       height: 200rpx;
       width: 100%;
@@ -260,6 +308,7 @@ let uploadImg = () => {
       grid-template-columns: repeat(3, 1fr);
       grid-auto-flow: row;
       gap: 20rpx;
+
       .up-image {
         width: 100%;
         background-color: white;
@@ -267,6 +316,7 @@ let uploadImg = () => {
         justify-content: center;
         align-items: center;
         font-size: 100rpx;
+
         image {
           height: 100%;
           width: 100%;
@@ -275,11 +325,13 @@ let uploadImg = () => {
       }
     }
   }
+
   .btns {
     display: flex;
     gap: 100%;
     justify-content: center;
     align-items: center;
+
     button {
       width: 600rpx;
     }
