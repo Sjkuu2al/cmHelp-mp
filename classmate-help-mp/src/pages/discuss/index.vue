2450c75e95d2855ad631c856e0cae424d54e1727
郑裕城 <yucheng_z@gzpionner.cn>
2024-04-07 18:23:00 +0800

    feat: 帖子列表页

--- classmate-help-mp/src/pages/discuss/index.vue
+++ classmate-help-mp/src/pages/discuss/index.vue
@@ -1,21 +1,27 @@
 <template>
   <view class="discuss-list">
-    <view class="type-bar">
-      <view :class="check === 1 ? 'check tab' : 'tab'" @click="changeCheck(1)">
-        最新
+    <div class="topbar">
+      <div class="left">
+        <view class="search-box">
+          <input class="search-input" placeholder="请输入关键词" v-model="keyword" />
+          <uni-button type="primary">搜索</uni-button>
+        </view>
+      </div>
+      <view class="right">
+        <view :class="check === 1 ? 'check tab' : 'tab'" @click="changeCheck(1)">
+          最新
+        </view>
+        <view :class="check === 2 ? 'check tab' : 'tab'" @click="changeCheck(2)">
+          热议
+        </view>
       </view>
-      <view :class="check === 2 ? 'check tab' : 'tab'" @click="changeCheck(2)">
-        热议
-      </view>
-    </view>
-    <view
-      class="card"
-      v-for="item in discussList"
-      :key="item.id"
-      @click="openDetail(item.id)"
-    >
+    </div>
+    <!--  -->
+    <view class="card" v-for="item in discussList" :key="item.id" @click="openDetail(item.id)">
       <view class="pInfo">
-        <view class="pImg"><image src="@/static/11.jpg" /></view>
+        <view class="pImg">
+          <image src="@/static/11.jpg" />
+        </view>
         <view class="pUser">{{ item.ownerName }}</view>
       </view>
 
@@ -24,9 +30,9 @@
         <image src="@/static/i-comment.png"></image>
         <view class="comment-count">33</view>
       </view>
-      </view> -->
     </view>
-    <view class="createbtn" @click="toCreate()">+</view>
+  </view>
+  <view class="createbtn" @click="toCreate()">+</view>
 </template>
 
 <script setup lang="ts">
@@ -37,6 +43,13 @@ let check = ref<number>(1);
 let changeCheck = (value: number) => {
   check.value = value;
 };
+
+// 搜索款
+let keyword = ref('123')
+
+
+
+// 最新、最热帖切换
 watch(check, (newVal) => {
   console.log(newVal);
   if (newVal === 1) {
@@ -75,7 +88,7 @@ onMounted(async () => {
     res.forEach((item: any) => {
       discussList.value.push(item);
     });
-   
+
   });
 });
 onReachBottom(async () => {
@@ -102,26 +115,66 @@ onReachBottom(async () => {
   display: flex;
   flex-direction: column;
   gap: 40rpx;
-  .type-bar {
+
+  .topbar {
     display: grid;
-    grid-template-columns: 1fr 1fr;
-    border-radius: 10rpx;
-    border: 2rpx solid black;
-    height: 120rpx;
-    .tab {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      padding: 20rpx;
+    grid-template-columns: 8fr 2fr;
+    height: 160rpx;
+    gap: 40rpx;
+
+    .left {
+      display: grid;
+      grid-template-rows: 1fr 1fr;
+      border-radius: 10rpx;
+      background-color: white;
+      box-shadow: 4rpx 4rpx 4rpx rgb(241, 241, 241);
+      height: 100%;
+
+      .search-box {
+        display: flex;
+        align-items: center;
+        height: 60rpx;
+        padding: 30rpx 10rpx 0rpx;
+
+        .search-input {
+          border-radius: 50rpx;
+          border: 2px solid rgb(223, 223, 223);
+          background-color: rgb(240, 239, 239);
+          padding-left: 40rpx;
+   
+        }
+        button{
+          height: 100%;
+          font-size: 24rpx
+        }
+      }
     }
-    .check {
-      background-color: rgb(166, 166, 245);
-      color: white;
-    }
-    .new {
-      border-right: 2rpx solid gray;
+
+    .right {
+      display: grid;
+      grid-template-rows: 1fr 1fr;
+      background-color: rgb(250, 250, 250);
+      border-radius: 10rpx;
+      border: 1rpx solid rgb(194, 194, 194);
+      height: 100%;
+
+      .tab {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+
+      .check {
+        background-color: rgb(166, 166, 245);
+        color: white;
+      }
+
+      .new {
+        border-right: 2rpx solid gray;
+      }
     }
   }
+
   .card {
     width: 100%;
     display: flex;
@@ -130,10 +183,12 @@ onReachBottom(async () => {
     border-radius: 10rpx;
     box-shadow: 10rpx 5rpx 10rpx rgb(232, 232, 232);
     padding: 10rpx 10rpx 30rpx 10rpx;
+
     .pInfo {
       display: flex;
       align-items: center;
       padding-top: 10rpx;
+
       .pImg {
         height: 100rpx;
         width: 100rpx;
@@ -144,29 +199,34 @@ onReachBottom(async () => {
           border-radius: 50rpx;
         }
       }
+
       .pUser {
         font-size: 48rpx;
         font-weight: bolder;
         text-indent: 0.4em;
       }
     }
+
     .pContent {
       margin-top: 10rpx;
       font-size: 36rpx;
       text-indent: 2em;
     }
+
     .comment {
       display: flex;
       justify-content: flex-end;
       align-items: center;
       padding-top: 10rpx;
       padding-right: 40rpx;
+
       image {
         height: 50rpx;
         width: 50rpx;
       }
     }
   }
+
   .createbtn {
     display: flex;
     justify-content: center;
